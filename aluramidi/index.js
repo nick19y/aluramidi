8a2ea8c7e3981215254b6454be7b7b85fbce16b2
Nícolas de Godoi <97116882+nick19y@users.noreply.github.com>
2023-07-05 09:56:46 -0300

    anotações sobre github

--- aluramidi/index.js
+++ aluramidi/index.js
@@ -35,4 +35,23 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     // digitar o "." 3 vezes em qualquer página no giithub faz com que o arquivo seja aberto em um editor vscode na web
     // a tendência é começar a rodar esses códigos no cloud msm
 
+    // git log --oneline mostra todos os commits do projeto
+
+    // git clone apaga todo o repositório e coloca a versão do projeto do github
+
+    // git pull link é usado para ver se tem alguma alteração no projeto, acredito q ele tb faça a alteração no repositório local
+
+    // git status mostra os arquivos modiicados no repositório local com relação ao remoto
+
+    // dá para usar o terminal do vs code p fazer commits tb
+
+    // diff é aquilo que foi modificado no arquivo, isso pode ser consultado na msg do commit do arquivo no github
+
+    // git commit . -m "" faz o commit em todo o arquivo
+
+    // git restore --source 470150d volta para essa versão do arquivo com todos os seus arquivos
+
+    // git restore --source 470150d app.js modifica o arquivo app.js do commit 470150d
+    // depois diso precisa fazer um commit avisando q voltou a versão e colocar um push
+
 }
